00e5743b1604e725266bf2e31a2317a67274ec05
bireswarghosh <116499672+bireswarghosh@users.noreply.github.com>
2023-12-23 11:20:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import session from "express-session";
 import cookieParser from "cookie-parser";
 import passport from "passport";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
-import cors from "cors";
+// import cors from "cors";
 
 const app = express();
 export default app;
@@ -20,11 +20,11 @@ app.use(
     resave: false,
     saveUninitialized: false,
 
-    cookie: {
-      secure: process.env.NODE_ENV === "development" ? false : true,
-      httpOnly: process.env.NODE_ENV === "development" ? false : true,
-      sameSite: process.env.NODE_ENV === "development" ? false : "none",
-    },
+    // cookie: {
+    //   secure: process.env.NODE_ENV === "development" ? false : true,
+    //   httpOnly: process.env.NODE_ENV === "development" ? false : true,
+    //   sameSite: process.env.NODE_ENV === "development" ? false : "none",
+    // },
   })
 );
 app.use(cookieParser());
@@ -35,13 +35,13 @@ app.use(
   })
 );
 
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: process.env.FRONTEND_URL,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
 
 app.use(passport.authenticate("session"));
 app.use(passport.initialize());
