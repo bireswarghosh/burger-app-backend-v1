617b8127ab05063f721aefb3908fb07a02dd1218
bireswarghosh <116499672+bireswarghosh@users.noreply.github.com>
2023-12-23 11:31:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -57,5 +57,8 @@ import orderRoute from "./routes/order.js";
 app.use("/api/v1", userRoute);
 app.use("/api/v1", orderRoute);
 
+
+app.use(express.static("build"));
+
 // Using Error Middleware
 app.use(errorMiddleware);
